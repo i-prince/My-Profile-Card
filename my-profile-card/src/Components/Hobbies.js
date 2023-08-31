7b7cb7b50744670cf0026b7c38664af39d53d5db
Levy <edgarlevy81@gmail.com>
2023-08-31 16:10:21 +0300

    2nd Commit

---
+++ my-profile-card/src/Components/Hobbies.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Hobbies() {
+  return (
+    <div>
+      {/* Hobbies content */}
+    </div>
+  );
+}
+
+export default Hobbies;
---
+++ my-profile-card/src/Components/ProfileCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ProfileCard() {
+  return (
+    <div>
+      {/* ProfileCard content */}
+    </div>
+  );
+}
+
+export default ProfileCard;
