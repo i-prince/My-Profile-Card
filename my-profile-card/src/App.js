0730803e15c46fe7cb4185dd94ceab84f9b0b4a8
Levy <edgarlevy81@gmail.com>
2023-08-31 11:52:40 +0300

    1st commit

--- my-profile-card/src/App.js
+++ my-profile-card/src/App.js
@@ -1,11 +1,17 @@
+import React from 'react';
 import './App.css';
-
+import Navbar from './Navbar';
+import ProfileCard from '/ProfileCard';
+import Hobbies from './Hobbies';
+import Footer from './Footer';
 function App() {
-  return (
-    <>
-    <h2>Add your components here</h2>
-    </>
-  )
+return (
+<div className="App">
+<Navbar />
+<ProfileCard />
+<Hobbies />
+<Footer />
+</div>
+);
 }
-
 export default App;
