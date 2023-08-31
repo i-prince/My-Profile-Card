67092b9ef37ff756b0fe3b1a74fd30610ed95603
preciousnjukia <preciousnjukia@gmail.com>
2023-08-31 15:58:10 +0300

    navbar done

--- my-profile-card/src/Components/Navbar.js
+++ my-profile-card/src/Components/Navbar.js
@@ -2,8 +2,26 @@ import React from 'react';
 
 function Navbar() {
   return (
-    <nav>
-      {/* Navbar content */}
+    <nav className="navbar">
+      <div className="navbar-title">MY PROFILE CARD</div>
+      <ul className="navbar-links">
+        <li>
+          <a href="/" className="navbar-link">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="/about" className="navbar-link">
+            About
+          </a>
+        </li>
+        <li>
+          <a href="/contacts" className="navbar-link">
+            Contacts
+          </a>
+        </li>
+      </ul>
+     
     </nav>
   );
 }
